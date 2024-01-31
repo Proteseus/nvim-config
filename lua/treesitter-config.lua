ac563eb5fa4c3ccbab897991a18b96d9ae94b7f1
Proteseus <lewiwui@gmail.com>
2024-01-31 14:37:13 +0300

    dart support

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -3,7 +3,7 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
   -- Add a language of your choice
-  ensure_installed = {"cpp", "python", "lua", "java", "javascript", "prisma", "flutter"},
+  ensure_installed = {"cpp", "python", "lua", "java", "javascript", "prisma", "dart"},
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
