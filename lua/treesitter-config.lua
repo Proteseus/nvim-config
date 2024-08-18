a16d6462e42662e04ff1f42b3933c5ac4cb9cf4b
Proteseus <lewiwui@gmail.com>
2024-08-18 19:10:47 +0300

    added parsers for markdown, markdown_inline and html

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -3,7 +3,7 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
   -- Add a language of your choice
-  ensure_installed = {"cpp", "python", "lua", "java", "javascript", "prisma", "dart", "go"},
+  ensure_installed = {"cpp", "python", "lua", "java", "javascript", "prisma", "dart", "go", "markdown", "markdown_inline", "html"},
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
