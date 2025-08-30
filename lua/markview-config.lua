b91c3ebd7497e1c2be1a66f9102ce6fede2b3adb
Proteseus <lewiwui@gmailcom>
2025-08-30 13:17:11 +0300

    markview syntax update

--- lua/markview-config.lua
+++ lua/markview-config.lua
@@ -1,14 +1,16 @@
 require("markview").setup({
-    modes = { "n", "i", "no", "c" },
-    hybrid_modes = { "i" },
-    -- Filetypes where the plugin is enabled
-    filetypes = { "markdown", "quarto", "rmd", "Avante" },
+    preview = {
+        modes = { "n", "i", "no", "c" },
+        hybrid_modes = { "i" },
+        -- Filetypes where the plugin is enabled
+        filetypes = { "markdown", "quarto", "rmd", "Avante" },
 
-    -- This is nice to have
-    callbacks = {
-        on_enable = function (_, win)
-            vim.wo[win].conceallevel = 2;
-            vim.wo[win].concealcursor = "nc";
-        end
+        -- This is nice to have
+        callbacks = {
+            on_enable = function (_, win)
+                vim.wo[win].conceallevel = 2;
+                vim.wo[win].concealcursor = "nc";
+            end
+        }
     }
 })
